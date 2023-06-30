6131b9a4808a6d5e18a2122024630bfae2820025
Hryhorii Kyriienko <hrhrkrnk@gmail.com>
2023-06-30 15:49:39 +0300

    task-01+

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,53 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryList.insertAdjacentHTML("afterbegin", galleryMarkup);
+galleryList.addEventListener("click", onGalleryListClick);
+
+function createGalleryMarkup(gallery) {
+  return gallery
+    .map(
+      ({ preview, original, description }) =>
+        (document.innerHTML = `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </li>
+      `)
+    )
+    .join("");
+}
+
+function onGalleryListClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const modalWindow = basicLightbox.create(
+    `<img src="${event.target.dataset.source}">`,
+    {
+      onShow: () => window.addEventListener("keydown", onPushKeyEscClose),
+      onClose: () => window.removeEventListener("keydown", onPushKeyEscClose),
+    }
+  );
+
+  modalWindow.show();
+
+  function onPushKeyEscClose(event) {
+    const ESC_KEY = "Escape";
+    if (event.code === ESC_KEY) {
+      modalWindow.close();
+    }
+  }
+}
